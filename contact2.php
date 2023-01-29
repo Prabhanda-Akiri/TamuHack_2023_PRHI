811280eff56acd32ec4419237c0a42caecc668d6
Siddharth Devulapalli <siddharthd@client-10-229-11-85.tamulink.tamu.edu>
2023-01-29 08:25:01 -0600

    fixing bugs

---
+++ contact2.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Sign-in User</title>
+
+    <!-- Bootstrap -->
+    <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.1/examples/cover/cover.css" rel="stylesheet">
+  <style type="text/css">
+     .form-signin {
+      max-width: 330px;
+      padding: 15px;
+      margin: 0 auto;
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin .checkbox {
+        font-weight: normal;
+      }
+      .form-signin .form-control {
+        position: relative;
+        height: auto;
+        -webkit-box-sizing: border-box;
+           -moz-box-sizing: border-box;
+                box-sizing: border-box;
+        padding: 10px;
+        font-size: 16px;
+      }
+      .form-signin .form-control:focus {
+        z-index: 2;
+      }
+
+      .form-signin input[type="email"] {
+        margin-bottom: 10px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+      .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+      }
+  </style>
+  <link rel="icon" href="cp2.png">
+  </head>
+
+  <body class="text-center">
+
+   <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column" style='background-color: #7c917f;max-width: 80em;'>
+      <header class="masthead mb-60">
+        <h3 class="masthead-brand" style="font-family: 'Merienda'; font-size: 38px;"><b>INPAT</b></h3>
+        <div class="inner">
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link" href="nav_home.php">Home</a>
+            <a class="nav-link" href="u_login.php" id="login">Login</a>
+            <a class="nav-link" href="user_signup.php" id="reg">Register</a>
+            <a class="nav-link" href="contact2.php" id="contact">Contact Us</a>
+          </nav>
+        </div>
+		<br>
+		<!--img src="cp2.png" style="max-width: 36%;height: auto;"-->
+      </header>
+    
+      <main role="main" class="inner cover" id="Main">
+        <div id="display">	
+			    <div class="mastfoot mt-auto">
+            
+          </div>
+        </div>
+        <h1 class="cover-heading">Contact us:</h1>
+        <br>
+        <br>
+        <address>
+          <h5 class="name"> Makers of INPAT</h5>
+          <a href="mailto:">jim@rock.com</a><br>
+          <a href="tel:+13115552368">(311) 555-2368</a>
+        </address>
+        <address>
+          <h5 class="name"> Makers of INPAT</h5>
+          <a href="mailto:">jim@rock.com</a><br>
+          <a href="tel:+13115552368">(311) 555-2368</a>
+        </address>
+        <address>
+          <h5 class="name"> Makers of INPAT</h5>
+          <a href="mailto:">jim@rock.com</a><br>
+          <a href="tel:+13115552368">(311) 555-2368</a>
+        </address>
+        <address>
+          <h5 class="name"> Makers of INPAT</h5>
+          <a href="mailto:">jim@rock.com</a><br>
+          <a href="tel:+13115552368">(311) 555-2368</a>
+        </address>
+      </main>
+    </div>
+  </body>
+</html>
+
